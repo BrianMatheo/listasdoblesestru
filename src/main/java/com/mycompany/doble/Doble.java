76dc8c8dfffbcaa6399d9823dfdc16dc89cd20b7
Brian Matheo <bmalvarezp@ufpso.edu.co>
2024-09-27 07:30:30 -0500

    v1

---
+++ src/main/java/com/mycompany/doble/Doble.java
@@ -0,0 +1,60 @@
+package com.mycompany.doble;
+
+import javax.swing.JOptionPane;
+
+public class Doble {
+
+    NodoDoble inicio;
+    Doble(){
+        inicio = null;
+    }
+    public void insertarInicio(String datos){
+        NodoDoble nuevo = new NodoDoble();
+        nuevo.setAnterior(null);
+        nuevo.setSiguiente(null);
+        nuevo.setDatos(datos);
+        
+        if(inicio != null){
+            
+            nuevo.setSiguiente(inicio);
+            inicio.setAnterior(nuevo);
+        }
+        inicio = nuevo;
+    }
+    
+    public void imprimir(){
+        
+        NodoDoble temporal = inicio;
+        if(inicio == null){
+            JOptionPane.showMessageDialog(null, "No hay nadie en la lista");
+        }else{
+            while(temporal != null){
+                JOptionPane.showMessageDialog(null, "Datos: " + temporal.getDatos());
+                temporal = temporal.getSiguiente();
+            }
+        }
+    }
+    
+    public void insertarFinal(String datos){
+        NodoDoble nuevo = new NodoDoble();
+        nuevo.setAnterior(null);
+        nuevo.setSiguiente(null);
+        nuevo.setDatos(datos);
+        
+        if(inicio != null){
+            nuevo.setSiguiente(inicio);
+            inicio.setAnterior(nuevo);
+            }else{
+            NodoDoble temporal = inicio;
+            while(temporal.getSiguiente() != null){
+                temporal = temporal.getSiguiente();
+            }
+            temporal.setSiguiente(nuevo);
+            nuevo.setSiguiente(temporal);
+        }
+        //insertar en una posicion dada
+        //insertar antes de una posicion dada
+        //eliminar un nodo dado
+    }
+    
+}
---
+++ src/main/java/com/mycompany/doble/NodoDoble.java
@@ -0,0 +1,48 @@
+package com.mycompany.doble;
+public class NodoDoble {
+
+    /**
+     * @return the anterior
+     */
+    public NodoDoble getAnterior() {
+        return anterior;
+    }
+
+    /**
+     * @param anterior the anterior to set
+     */
+    public void setAnterior(NodoDoble anterior) {
+        this.anterior = anterior;
+    }
+
+    /**
+     * @return the siguiente
+     */
+    public NodoDoble getSiguiente() {
+        return siguiente;
+    }
+
+    /**
+     * @param siguiente the siguiente to set
+     */
+    public void setSiguiente(NodoDoble siguiente) {
+        this.siguiente = siguiente;
+    }
+
+    /**
+     * @return the datos
+     */
+    public String getDatos() {
+        return datos;
+    }
+
+    /**
+     * @param datos the datos to set
+     */
+    public void setDatos(String datos) {
+        this.datos = datos;
+    }
+    private NodoDoble anterior;
+    private NodoDoble siguiente;
+    private String datos;
+}
